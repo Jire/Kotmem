c59449decae52ab74d0faf24c193f945cc5df511
Jire <technology@live.com>
2015-12-02 03:02:39 -0500

    Added lambda support for processByID and processByName

--- src/main/kotlin/kotmem/Kotmem.kt
+++ src/main/kotlin/kotmem/Kotmem.kt
@@ -4,7 +4,19 @@ import kotmem.unsafe.*
 
 fun processByID(id: Int) = Process(openProcess(id))
 
+inline fun processByID(id: Int, action: (Process) -> Unit): Process {
+	val process = processByID(id)
+	action.invoke(process)
+	return process
+}
+
 fun processByName(name: String): Process = processByID(pidByName(name))
+
+inline fun processByName(name: String, action: (Process) -> Unit): Process {
+	val process = processByName(name)
+	action.invoke(process)
+	return process
+}
 
 fun keyState(keyCode: Int) = User32.GetKeyState(keyCode)
 

2a883b3ce503d45bbe1274b156c1b888a527995d
Jire <technology@live.com>
2015-12-09 14:43:42 -0500

    Forgot to make the functions accept T: Any generic type

--- src/main/kotlin/org/jire/kotmem/Module.kt
+++ src/main/kotlin/org/jire/kotmem/Module.kt
@@ -8,12 +8,12 @@ class Module(val process: Process, val unsafe: UnsafeModule) {
 
 	val address by lazy { resolveModuleAddress(unsafe) }
 
-	operator inline fun <reified T> get(offset: Long) = process.get<T>(address + offset)
+	operator inline fun <reified T : Any> get(offset: Long) = process.get<T>(address + offset)
 
-	operator inline fun <reified T> get(offset: Int): T = get(offset.toLong())
+	operator inline fun <reified T : Any> get(offset: Int): T = get(offset.toLong())
 
-	operator inline fun <reified T> set(offset: Long, data: T) = process.set(address + offset, data)
+	operator inline fun <reified T : Any> set(offset: Long, data: T) = process.set(address + offset, data)
 
-	operator inline fun <reified T> set(offset: Int, data: T): Unit = set(offset.toLong(), data)
+	operator inline fun <reified T : Any> set(offset: Int, data: T): Unit = set(offset.toLong(), data)
 
 }
